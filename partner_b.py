42311d2123a713429f1fe88030f9759f8d3576b2
alejandra-martinez2 <alemtz0507@gmail.com>
2026-06-30 11:12:24 -0700

    1st task

--- partner_b.py
+++ partner_b.py
@@ -32,10 +32,16 @@ scores = ['88', '95', 'absent', '72', 'n/a', '84']
 
 valid = []
 
+
 for i, s in enumerate(scores):
-    score = int(s)            # <-- this line crashes on bad strings
-    print(f'Score [{i}]: {score}')
-    valid.append(score)
+    try:
+        score = int(s)            # <-- this line crashes on bad strings
+        print(f'Score [{i}]: {score}')
+        valid.append(score)
+    except(ValueError):
+        print(f'[Index {i}] Skipped bad score: {s}')
+
+
 
 print(f'Total of valid scores: {sum(valid)}')
 print(f'Count of valid scores: {len(valid)}')
